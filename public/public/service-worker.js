7b039171456c190237e29d6dd401995e167c6e1d
tacionlopes <tacionlopes@gmail.com>
2025-05-16 14:36:36 -0300

    Delete public/public directory

--- public/public/service-worker.js
+++
@@ -1,7 +0,0 @@
-self.addEventListener("install", (e) => {
-  console.log("Service Worker: Instalado");
-});
-
-self.addEventListener("fetch", (e) => {
-  console.log("Service Worker: Buscando", e.request.url);
-});
